60836a291164e9112c994d8cd21652355377d94a
Sritji <111283306+Sritji@users.noreply.github.com>
2026-08-08 14:40:13 +0100

    Add files via upload

---
+++ day_one/band_name_generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the band name game")
+city = input("Which city did you grow up in?")
+pet = input("What is the name of your pet?")
+print("your band name could be: " + city + " " + pet)
+
---
+++ day_one/day_one.py
@@ -0,0 +1,25 @@
+# Print
+print("Hello Wworld!")
+
+print("i am" + " " + "Paul")
+
+#Input function
+
+input("What is your name?")
+
+print("My name is" + " " + input("What is your name?") + "!")
+
+#Variables
+
+name = "Peter"
+print(name)
+
+#length check
+
+print(len(input("What is your name?")))
+
+# using variables to check length
+
+username = input("What is your username?")
+length = len(username)
+print(length)
