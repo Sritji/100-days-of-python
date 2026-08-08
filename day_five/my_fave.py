0b561e477edcd4a6983d32fc67c40292297b2e54
Sritji <111283306+Sritji@users.noreply.github.com>
2026-08-08 20:49:44 +0100

    Delete day_five directory

--- day_five/my_fave.py
+++
@@ -1,1 +0,0 @@
-my_fav_number = 234
--- day_five/who_will_pay_the_bill.py
+++
@@ -1,9 +0,0 @@
-import random
-friends = ["Ritji", "Barnabas", "Prince", "Precious", "Bright"]
-
-#ption 1
-print(random.choice(friends))
-
-#option 2
-random_index = random.randint(0,4)
-print(friends[random_index])
